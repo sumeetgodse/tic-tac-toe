31c424ef33b827a72d0984ccb3d57d13e2173be4
Sumeet Godse <sumeetgodse1997@gmail.com>
2024-11-15 21:18:10 +0530

    update : checking in after long time

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
+## Tic-Tac-Toe
 
+### This is a simple tic-tac-toe application built using react.
+
+### Run locally -
+
+- Clone this repo
+- Run `yarn install`
+- Run `yarn start`
+
+#### App should now be running on `http://localhost:3000/`
+
+#### Visit live app - https://react-tictactoe-gold.vercel.app/
--- src/components/SquareComponent.js
+++ src/components/SquareComponent.js
@@ -1,11 +1,11 @@
 import React from "react";
 import './../App.css';
 
-function SquareComponent(props) {
-    const classes=props.className ? `${props.className} square` : `square`;
+const SquareComponent = (props) => {
+  const classes = props.className ? `${props.className} square` : `square`;
   return (
-    <span className={classes +  (props.state === "X" ? ` x` : ` o`)} onClick={props.onClick}>
-        {props.state}
+    <span className={classes + (props.state === "X" ? ` x` : ` o`)} onClick={props.onClick}>
+      {props.state}
     </span>
   );
 }
